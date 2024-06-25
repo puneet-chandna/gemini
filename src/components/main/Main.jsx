045fb04feb1617cd5f2f28f6ec93c2bf1f5ce2ad
[Puneet] <puneetchandna21@gmail.com>
2024-06-25 13:22:47 +0530

    updated main.jsx and main.css file

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -8,6 +8,37 @@ const Main = () => {
                 <p>Gemini</p>
                 <img src={assets.user_icon} alt='profile'/>
             </div>
+
+            <div className="main_container">
+                <div className="greet">
+                    <p> <span> Hello, Dev</span></p>
+                    <p>How can I help you today?</p>
+                </div>
+                <div className="cards">
+                    <div className="card">
+                        <p> suggest a place in india for a road trip</p>
+                        <img src ={assets.compass_icon} alt=''/>
+
+                    </div>
+
+                    <div className="card">
+                        <p>breifly summerize  the concept of blockchain indexing</p>
+                        <img src ={assets.bulb_icon} alt=''/>
+
+                    </div>
+                    <div className="card">
+                        <p> give me some Ideas for doins an online event for my college club</p>
+                        <img src ={assets.message_icon} alt=''/>
+
+                    </div>
+                    <div className="card">
+                        <p>Fix this code and make it more efficient</p>
+                        <img src ={assets.code_icon} alt=''/>
+
+                    </div>
+        
+                </div>
+            </div>
         </div>
     );
 };
